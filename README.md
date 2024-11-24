941b46cfa7ae1eec5c34061b4285424ac0cf046b
Bkotay <bkotay4@gmail.com>
2024-11-24 15:59:04 -0500

    Finished Product

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Hello , my name is Blamah Kotay. Welcomee to the BK Budget Tracker.
 
 This tracker was made through HTML, SASS styling and javaScript. This tracker increased my understanding of SASS and OOP (Object Oriented Programming) through the use of variables, mixins, objects, properties and first class function.
 
-The use of SASS was made useful and more efficient to style the tracker throug thee use of making variables and nesting to have more order and quicker code to display. The use of the class and DOM manipulation was made useful to seperate the expenses and incomes of the tracker and to directly impact the display of the desired budget outcome.
+There are inputs and buttons to calculate the money made/loss with the most recent incomes and expenses displayed after each submission.
 
-Refresh the page if you want to start the budget over. This bu
+The use of SASS was made useful and more efficient to style the tracker through the use of making variables and nesting to have more order and quicker code to display. The use of the class and DOM manipulation was made useful to seperate the expenses and incomes of the tracker and to directly impact the display of the desired budget outcome.
 
-Enjoy the website!
+Enjoy the BK BUDGET TRACKER! Available on all devices.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// First, I will make a class called Total that will store the 
+// First, I will make a class called Total that will store a constructor method that will create and initialize the objects being used. As said in the requirements I sepereated the this.income and this.expense with arrays[].
 
 class Total {
     constructor(){
@@ -6,8 +6,10 @@ class Total {
      this.expense = [];
      this.savings = 0;
      this.loss = 0;
+     this.final = 0;
     }
     
+//takeIncome will be used to push this.income into the function and this.savings to add or equal to the parameter amount but only if the amount of the income is a number or greater than zero. if these two are not valid then a alert will print out.
 
     takeIncome(amount) {
         if (!isNaN(amount) && amount > 0) {
@@ -19,6 +21,8 @@ class Total {
         }
     }
 
+    // takeExpense will be used similar to the way takeIncome is used. The alert will print out if the amount of the expense is a number or if its greater than zero. If these two are not valid then a alert will print out.
+
     takeExpense(amount) {
         if (!isNaN(amount) && amount > 0) {
             this.expense.push(amount);
@@ -29,13 +33,17 @@ class Total {
         }
     }
 
+    //renewBudget contains the HTML ID elements that will show the text through the method of textContent of the calculations from the objects from the class Total. We sttack these ID's through getElementBYID and set it equal to the price of either the income, expense or final balance. toFixed is used to set certain decimal points. 
+
     renewBudget() {
-        document.getElementById("earnings-number").textContent = this.savings;
-        document.getElementById("expenses-number").textContent = this.loss;
-        document.getElementById("final-number").textContent = this.savings - this.loss;
+        this.final = this.savings - this.loss;
+        document.getElementById("earnings-number").textContent = this.savings.toFixed(2);
+        document.getElementById("expenses-number").textContent = this.loss.toFixed(2);
+        document.getElementById("final-number").textContent = this.final.toFixed(2)
     }
 }
 
+//We enscapsulate the data by creating a new class which allows certain functionalities and created new objects if needed to leverage principles such as inheritance to reduce the use of redundant code. We attack the income button and addEVentListener to listen for a click so it can run the funcftion. The use of prevent default is a bug that prevents the page froma utomatically refreshing once the button is clicked. We use innerTect to print it out the price and template literal after you have hit the income or expense button after typing the name of the description and amount.
 
 const total = new Total ();
 
@@ -46,10 +54,7 @@ document.getElementById("income-button").addEventListener
     const incAmount = parseFloat(document.getElementById("income-money").value);
     total.takeIncome(incAmount);
     const message = document.querySelector('.message')
-message.innerText = `Updated Budget Summary: The name of the most recent income is ${incDescription} and the most recent income amount is ${incAmount}.`
-
-
-    event.target.reset();
+message.innerText = `The name of the most recent source of income was ${incDescription} and the amount of the that income was $ ${incAmount}.`
 })
 
 document.getElementById("expense-button").addEventListener("click", function(event){
@@ -57,9 +62,8 @@ event.preventDefault();
 const expDescription = document.getElementById("expense-description").value;
 const expAmount = parseFloat(document.getElementById("expense-money").value);
 total.takeExpense(expAmount);
-const message = document.querySelector('.message')
-message.innerText = `Updated Budget Summary: The name of the most recent expense is ${expDescription} and the most recent expense amount is ${expAmount}.`
-event.target.reset();
+const message = document.querySelector('.message-two')
+message.innerText = `The name of the most recent expense was ${expDescription} and the amount of that expense was $ ${expAmount}.`
 })
 
 
